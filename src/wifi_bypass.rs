6068e9d217abc8c85687cf3b542ce18431305e22
Brendan McShane <brendanmcs7@gmail.com>
2023-10-16 18:54:50 -0400

    Add docs

--- src/wifi_bypass.rs
+++ src/wifi_bypass.rs
@@ -1,4 +1,5 @@
-//! 
+//! Bypass the check that prevents sending deauth packets
+//! Credit to: https://github.com/risinek/esp32-wifi-penetration-tool/blob/master/components/wsl_bypasser/README.md
 #[no_mangle]
 pub fn ieee80211_raw_frame_sanity_check() -> i32{
     return 0;
